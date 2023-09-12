f4657f9f2942dd2d2418632fc6711e2af46d3a40
LSmuts <132968213+LSmuts@users.noreply.github.com>
2023-09-12 13:40:07 +1000

    Second Commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import topicsReducer from "../features/topics/topicsSlice";
+import quizzesReducer from "../features/quizzes/quizzesSlice";
 
 export default configureStore({
   reducer: {
